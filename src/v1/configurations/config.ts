3ae2bf06dc5b41d6998e5d92f4dc1d899448c670
sahiljamwal <sahil.jamwal78625@gmail.com>
2025-05-08 21:24:12 +0530

    chore(main): Added readme.md

--- src/v1/configurations/config.ts
+++ src/v1/configurations/config.ts
@@ -2,13 +2,7 @@ import { config } from "dotenv";
 config();
 
 // Validate required environment variables
-const requiredEnvVars = [
-  "NODE_ENV",
-  "PORT",
-  "MONGO_DB_URL",
-  "REDIS_ENDPOINT",
-  "JWT_SECRET",
-];
+const requiredEnvVars = ["NODE_ENV", "PORT", "MONGO_DB_URL", "JWT_SECRET"];
 
 for (const envVar of requiredEnvVars) {
   if (!process.env[envVar]) {
@@ -20,7 +14,6 @@ const configuration = {
   env: process.env.NODE_ENV || "dev",
   port: +(process.env.PORT || 3000),
   mongoDbUrl: process.env.MONGO_DB_URL!,
-  redisEndpoint: process.env.REDIS_ENDPOINT!,
   jwtSecret: process.env.JWT_SECRET!,
 };
 
